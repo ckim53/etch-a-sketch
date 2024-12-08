e09da85c757e3d26574c263c7c2083fb8e39753d
Christina <184446177+ckim53@users.noreply.github.com>
2024-12-07 16:08:19 -0800

    Implement new grid functionality

--- scripts.js
+++ scripts.js
@@ -1,25 +1,55 @@
+let squareSize = 60;
+let newSize = 0;
+
 function createSquare(size) {
     const square = document.createElement("div");
     square.style.width = size + "px";
     square.style.height = size + "px";;
-    square.style.margin = "1px";
     square.style.backgroundColor = "blue";
     return square;
 }
 
-let row = 0;
-let col = 0;
-const squareSize = 70;
-
-const container = document.querySelector(".container");
-
-for (row; row < 16; row++) {
-    col = 0;
-    for (col; col < 16; col++) {
-        const square = createSquare(squareSize);
-        square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "green";
-        })
-        container.appendChild(square);
+function cleanUp() {
+    const container = document.querySelector(".container");
+    const squares = document.querySelectorAll(".square");
+    for (node of squares){
+        container.removeChild(node);
     }
 }
+
+function startGrid(size) {
+    let row = 0;
+    let col = 0;
+    squareSize = 960 / size;
+    const container = document.querySelector(".container");
+    for (row; row < size; row++) {
+        col = 0;
+        for (col; col < size; col++) {
+            const square = createSquare(squareSize);
+            square.classList.add("square");
+            square.addEventListener("mouseenter", () => {
+                square.style.backgroundColor = "green";
+            })
+            container.appendChild(square);
+        }
+    }
+    buttonCheck();
+}
+
+function buttonCheck() {
+    console.log("buttoncheck");
+    const button = document.querySelector("button");
+    button.textContent = "Start a new grid!";
+    button.addEventListener("click", () => {
+        newSize = prompt("Enter the number of squares per side for the new grid.");
+        while (newSize > 100) {
+            newSize = prompt("The maximum is 100 squares. Please Enter a lower value.")
+        }
+        cleanUp();
+        startGrid(newSize); }
+    );
+}
+
+startGrid(16);
+
+
